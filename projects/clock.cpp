671e599cf5553e274699d7ae6436a7ab66cf8758
Danny Nguyen <nguyenduongpa@gmail.com>
2025-05-17 10:16:41 -0700

    adding more features to clock

--- projects/clock.cpp
+++ projects/clock.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <iomanip>
 #include <windows.h>
+#include <conio.h>
+
 using namespace std;
 
 int runClock(int h, int m, int s) {
     while (true) {
         system("cls");
         cout << setw(2) << setfill('0') << h << ":" << setw(2) << setfill('0') << m << ":" << setw(2) << setfill('0') << s << endl;
+        cout << "\nPress 'q' to re-enter time, ESC to exit program" << endl;
         Sleep(1000);
         s++;
         if (s > 59) {
@@ -20,12 +23,20 @@ int runClock(int h, int m, int s) {
         if (h > 23) {
             h = 0;
         }
+        if (_kbhit()) {
+            char ch = _getch();
+            if (ch == 'q' || ch == 'Q') {
+                return 1;
+            }
+            if (ch == 27) {
+                return 2;
+            }
+        }
     }
 }
 
 int main() {
-    int h, m, s, a, err;
-    err = a = 0;
+    int h, m, s;
     while (true) {
         cout << "Let's set the current time!" << endl;
         cout << "Enter hour: " << endl;
@@ -34,11 +45,16 @@ int main() {
         cin >> m;
         cout << "Enter seconds: " << endl;
         cin >> s;
-        if (h < 24 && m < 60 && s < 60) {
-            runClock(h, m, s);
+        if (h < 24 && m < 60 && s < 60  && h >= 0 && m >= 0 && s >= 0) {
+            int result = runClock(h, m, s);
+            if (result == 2) break;
         } else {
             system("cls");
+            cout << "Invalid time! Hours (0-23), Minutes (0-59), Seconds (0-59)" << endl;
+            Sleep(3000);
         }
     }
+    system("cls");
+    cout << "Program exited." << endl;
     return 0;
 }
